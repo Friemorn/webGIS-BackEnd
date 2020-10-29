55e06e3a9167a7e93f3df8e6bd30c0befe313ee5
Friemorn <dewondofriemorn.s4a@gmail.com>
2020-10-29 21:37:22 +0700

    crud point

--- src/routes/point.js
+++ src/routes/point.js
@@ -8,9 +8,9 @@ module.exports = router
 router.get("/", async (req, res) => {
   try {
     const allPoint = await pool.query("SELECT id_point, nama_point, geom FROM point")
-    res.json(allPoint.rows);
+    res.json(allPoint.rows)
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
 });
 
@@ -18,41 +18,39 @@ router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params
     const pointById = await pool.query("SELECT id_point, nama_point, geom FROM point WHERE id_point = $1", [id])
-    res.json(pointById.rows);
+    res.json(pointById.rows)
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
 });
 
 router.post("/", async (req, res) => {
   try {
-    const { id_point, nama_point, geom } = req.body;
-    const data = { id_point, nama_point, geom}
-    const insertPoint = await pool.query("INSERT INTO point (id_point, nama_point, geom) VALUES($1, $2, POINT($3)) RETURNING *", [data])
-    res.json(insertPoint.rows[0]);
+    const { id_point, nama_point, lat, long } = req.body
+    const insertPoint = await pool.query("INSERT INTO point (id_point, nama_point, geom) VALUES($1, $2, (ST_SetSRID(ST_MakePoint($3, $4),4326))) RETURNING *", [id_point, nama_point, lat, long])
+    res.json(insertPoint.rows[0])
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
 });
 
 router.patch("/:id", async (req, res) => {
   try {
-    const { id } = req.params;
-    const { id_point, nama_point, geom } = req.body;
-    const data = { id_point, nama_point, geom}
-    const updatePoint = await pool.query("UPDATE point SET id_point = $1, nama_point = $2, geom = $3 WHERE id_point = $1", [id, data]);
-    res.json("Point was Updated!");
+    const { id } = req.params
+    const { id_point, nama_point, lat, long } = req.body
+    const updatePoint = await pool.query("UPDATE point SET nama_point = $2, geom = (ST_SetSRID(ST_MakePoint($3, $4),4326)) WHERE id_point = $1", [id, nama_point, lat, long])
+    res.json("Point was Updated!")
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
 });
 
 router.delete("/:id", async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     const deletePoint = await pool.query("DELETE FROM point WHERE id_point = $1", [id])
-    res.json("Point was Deleted!");
+    res.json("Point was Deleted!")
   } catch (err) {
-    console.log(err.message);
+    console.log(err.message)
   }
 });
